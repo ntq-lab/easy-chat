4b49af6ca5644248970463fbe0552ce486bd9336
Ta Quoc Vuong <tqv.itvn@gmail.com>
2015-02-25 19:08:48 +0700

    Build chat app (unfinished)

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-var app = require('express')(),
-    http = require('http').Server(app),
-    io = require('socket.io')(http);
+var express = require('express'),
+    http = require('http');
+
+// create Express application
+var app = express();
+
+// serve static files
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-io.on('connection', function(socket) {
-    socket.on('chat message', function(msg) {
-        io.emit('chat message', msg);
-    });
+// create server
+var server = http.Server(app);
+
+// var io = socketIO.listen(server);
+
+// io.on('connection', function(socket) {
+//     socket.on('chat message', function(msg) {
+//         io.emit('chat message', msg);
+//     });
+// });
+
+server.listen(2222, function() {
+    console.log('Listening on port 2222');
 });
 
-http.listen(3000, function() {
-    console.log('Listening on port 3000');
-});
+// providing it with an already defined HTTP server so it can share the same TCP/IPport
+var chatServer = require('./lib/chat-server');
+
+chatServer.listen(server);
